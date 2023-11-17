72556876250492745859244723b0740a5c95595c
MosutLarisa <larisa.mosut@emanuel.ro>
2023-11-17 09:52:48 +0200

    oop methods

--- OopMethods/Program.cs
+++ OopMethods/Program.cs
@@ -4,11 +4,18 @@
     {
         static void Main(string[] args)
         {
+            int i = 10;
+
             ConsoleHelper.Print("test");
 
             ConsoleHelper.Print("label", "test");
 
             ConsoleHelper.Print("test", 3);
+
+            Console.WriteLine(MathHelper.Factorial(5));
+
+            int[] array = { 1, 2, 3, 4, 5 };
+            ArrayHelper.IterateAndPrint(array);
         }
     }
 }
